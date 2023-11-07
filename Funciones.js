6f7a1c31c9793cf2bb45e6b4b44f42b5f11291af
TheBabyRin <guzman378105@gmail.com>
2023-11-07 01:27:31 -0600

    mi repositorio

---
+++ Funciones.js
@@ -0,0 +1,43 @@
+var xhttp = new XMLHttpRequest();
+function Main() {
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+       document.getElementById("demo").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "Main.php", true);
+    xhttp.send();
+  }
+
+function LoadTable() {  
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+       document.getElementById("demo").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "TablaComparativa.php", true);
+    xhttp.send();
+}
+
+console.log("hola");
+
+
+function LoadDocumentation(){
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("demo").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "Documentation.php", true);
+  xhttp.send();
+}
+
+function LoadPortada(){
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("demo").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "Portada.php", true);
+  xhttp.send();
+}
